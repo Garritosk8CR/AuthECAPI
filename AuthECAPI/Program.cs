f4c4c44731a6610bde8415a649a515b5e1898f2b
Emilio Garro Rangel <emiliogarrorangelxboxone@hotmail.com>
2025-02-27 20:28:45 -0600

    AppDbContext injected as dependency

--- AuthECAPI/Program.cs
+++ AuthECAPI/Program.cs
@@ -1,5 +1,6 @@
 using AuthECAPI.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,9 @@ builder.Services.AddOpenApi();
 builder.Services
     .AddIdentityApiEndpoints<IdentityUser>()
     .AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DevDB")));
 
 var app = builder.Build();
 
