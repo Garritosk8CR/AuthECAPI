8b24571be01a5451e962b90f57bddf00b7ac1ed8
Emilio Garro Rangel <emiliogarrorangelxboxone@hotmail.com>
2025-02-28 01:49:31 -0600

    added AppUser to the db context to be recognised during migrations

--- AuthECAPI/Models/AppDbContext.cs
+++ AuthECAPI/Models/AppDbContext.cs
@@ -8,5 +8,7 @@ namespace AuthECAPI.Models
         public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
         {
         }
+
+        public DbSet<AppUser> AppUsers { get; set; }
     }
 }
