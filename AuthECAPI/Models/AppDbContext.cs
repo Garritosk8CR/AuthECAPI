43d9a7aa5f25e6cf63b8dbb8b22667d4be1bf2df
Emilio Garro Rangel <emiliogarrorangelxboxone@hotmail.com>
2025-02-27 20:18:24 -0600

    Add project files.

---
+++ AuthECAPI/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthECAPI.Models
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        protected AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+        }
+    }
+}
