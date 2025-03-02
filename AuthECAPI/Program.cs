4115827b2f676919eba1190494beb3331a8834e3
Emilio Garro Rangel <emiliogarrorangelxboxone@hotmail.com>
2025-03-01 19:12:31 -0600

    added nugget package for jwt tokens, configured authentication in program.cs

--- AuthECAPI/Program.cs
+++ AuthECAPI/Program.cs
@@ -1,8 +1,11 @@
 using AuthECAPI.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,6 +30,24 @@ builder.Services.Configure<IdentityOptions>(options =>
 builder.Services.AddDbContext<AppDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DevDB")));
 
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = 
+    x.DefaultChallengeScheme = 
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(y =>
+{
+    y.SaveToken = false;
+    y.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["appSettings:JWTSecret"]!)
+            ),
+        
+    };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -45,7 +66,7 @@ app.UseCors(
         .AllowCredentials()
 );
 #endregion
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
